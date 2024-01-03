1f31b10d80573bc9d10fadf031b6b29118b29867
Vanessa <zdperfectx@gmail.com>
2024-01-02 19:00:56 -0600

    completed solutions

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,14 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    const map = new Map();
+    const set = new Set();
+    a.forEach((element) => map.set(element));
+    b.forEach((element) => {
+        if (map.has(element)) {
+            set.add(element);
+        } 
+    })
+    const newArray = [...set];
+    return newArray;
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,18 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    const numbers = new Map();
+    const solution = new Map();
+    A.forEach((element) => numbers.set(element, element));
+    A.forEach((element) => {
+        const diff = N - element;
+        if (diff !== element && numbers.has(diff)) {
+            solution.set(Math.min(element, diff), Math.max(element, diff));
+        }
+    })
+    const newArray = [...solution];
+    return newArray;
+}
 
 module.exports = sumPairs;
